b7c1e14ce141420f27a4f7be3c13df952c1f3398
Arsalan Khalid <arslankhalid889@gmail.com>
2021-07-19 03:08:40 -0700

    meals component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import './MealsSummary.css'
+
+const MealsSummary = () => {
+    return (
+        <section className="summary">
+            <h2>Delicious Food, Delivered to you</h2>
+            <p>
+                Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home. 
+            </p>
+            <p>
+                All meals have been prepared with premium quality ingredients, and by experienced chefs !
+            </p>
+        </section>
+    );
+};
+
+export default MealsSummary;
