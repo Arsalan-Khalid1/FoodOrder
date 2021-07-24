e3d6d7a453027690a39972d30eaa079f5eb99e6a
Arsalan Khalid <arslankhalid889@gmail.com>
2021-07-24 12:41:14 -0700

    cart animation upon items change

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,31 @@
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import "./HeaderCartButton.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const CrTx = useContext(CartContext);
-
-  const numberOfCartItems = CrTx.items.reduce((currentNumber, item) => {
+  const { items } = CrTx;
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    } else {
+      setBtnIsHighlighted(true);
+      setTimeout(() => {
+        setBtnIsHighlighted(false);
+      }, 300);
+    }
+  }, [items]);
+
+  const btnClasses = `button ${btnIsHighlighted ? `bump` : ``}`;
+
   return (
-    <button className="button" onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className="icon">
         <CartIcon />
       </span>
